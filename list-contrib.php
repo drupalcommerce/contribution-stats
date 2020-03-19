942a6e5c8963d5925386b966d56be4fd9f5d280f
Bojan Zivanovic <bojanz@gmail.com>
2020-03-19 12:49:10 +0100

    list-contrib.php must use curl because d.o rejects requests without a user agent.

--- list-contrib.php
+++ list-contrib.php
@@ -2,7 +2,12 @@
 
 // We parse the HTML view because the REST endpoint has no obvious way
 // to filter by core version.
-$html = file_get_contents('https://www.drupal.org/project/project_module/index?project-status=full&drupal_core=7234');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.drupal.org/project/project_module/index?project-status=full&drupal_core=7234');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36');
+$html = curl_exec($ch);
+
 $doc = new DOMDocument();
 @$doc->loadHTML($html);
 $elements = $doc->getElementsByTagName('span');
