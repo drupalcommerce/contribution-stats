e379aa91db1924577bd3c23bdd81ea1bfe155d0a
Shawn McCabe <smccabe@acromediainc.com>
2021-11-23 14:17:39 -0800

    Refactor listcontrib to use API instead of scraping

--- src/ListContrib/ListContrib.php
+++ src/ListContrib/ListContrib.php
@@ -9,22 +9,33 @@ class ListContrib
     {
         $args = getopt("o::", ['output-format::']);
 
+        $commerce_modules = [];
+        
         // We parse the HTML view because the REST endpoint has no obvious way
         // to filter by core version.
         $client = new \GuzzleHttp\Client();
 
-        $response = $client->get('https://www.drupal.org/project/project_module/index?project-status=full&drupal_core=7234');
+        $url = 'https://www.drupal.org/api-d7/node.json?type=project_module';
+        
+        while($url) {
+            $response = $client->get($url);
+            $data = json_decode($response->getBody());
+            
+            $url = $data->next ?? null;
+            // The next links are broken in the API and we need to fix them manually
+            $url = str_replace('node?', 'node.json?', $url);
+            $modules = $data->list;
 
-        $html = $response->getBody();
-
-        $doc = new \DOMDocument();
-        @$doc->loadHTML($html);
-        $elements = $doc->getElementsByTagName('span');
-        $modules = [];
-
-        foreach ($elements as $element) {
-            if (strpos(strtolower($element->nodeValue), 'commerce') !== false) {
-                $modules[$element->nodeValue] = $element->firstChild->getAttribute('href');
+            foreach ($modules as $module) {
+                $name = $module->title;
+                $machine_name = $module->field_project_machine_name;
+                
+                // If it isn't a commerce module, we don't care about it.
+                if (strpos(strtolower($name), 'commerce') === false) {
+                    continue;
+                }
+                
+                $commerce_modules[$name] = $machine_name;
             }
         }
 
@@ -36,13 +47,13 @@ class ListContrib
         switch ($output_format) {
             case 'json':
                 print json_encode(
-                    $modules,
+                    $commerce_modules,
                     JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
                 );
                 break;
             case 'text':
-                print_r($modules);
-                print 'Total Modules: ' . count($modules) . PHP_EOL;
+                print_r($commerce_modules);
+                print 'Total Modules: ' . count($commerce_modules) . PHP_EOL;
                 break;
             default:
                 print 'The output format you specified is not valid';
